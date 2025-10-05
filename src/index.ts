b8a4374a403cf2ca7a4fe8fa3eb571cbd297ce7f
MuhammadRukon <muhammad.rukon242@gmail.com>
2025-10-05 16:52:41 +0600

    fix: vercel config

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import "dotenv/config";
 import { userRoutes } from "./routes/user-routes";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
